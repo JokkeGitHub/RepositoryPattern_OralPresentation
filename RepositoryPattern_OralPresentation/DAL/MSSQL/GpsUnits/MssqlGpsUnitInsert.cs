335687460ec6e2f85178e04b65e19c14b8f5d6c2
Joachim Hansen <skraldespand94@hotmail.com>
2023-09-05 07:15:08 +0200

    Changed datetime name to timestamp name

--- RepositoryPattern_OralPresentation/DAL/MSSQL/GpsUnits/MssqlGpsUnitInsert.cs
+++ RepositoryPattern_OralPresentation/DAL/MSSQL/GpsUnits/MssqlGpsUnitInsert.cs
@@ -13,14 +13,14 @@ namespace RepositoryPattern_OralPresentation.DAL.MSSQL.GpsUnits
             SqlCommand command = new SqlCommand();
             command.Connection = connection;
 
-            command.CommandText = @"INSERT INTO GpsUnit(id, name, active, latitude, longitude, altitude, datetime) VALUES (@id, @name, @active, @latitude, @longitude, @altitude, @datetime)";
+            command.CommandText = @"INSERT INTO GpsUnit(id, name, active, latitude, longitude, altitude, timestamp) VALUES (@id, @name, @active, @latitude, @longitude, @altitude, @timestamp)";
             command.Parameters.AddWithValue("@id", gpsUnit.ID);
             command.Parameters.AddWithValue("@name", gpsUnit.Name);
             command.Parameters.AddWithValue("@active", gpsUnit.Active);
             command.Parameters.AddWithValue("@latitude", gpsUnit.Latitude);
             command.Parameters.AddWithValue("@longitude", gpsUnit.Longitude);
             command.Parameters.AddWithValue("@altitude", gpsUnit.Altitude);
-            command.Parameters.AddWithValue("@datetime", gpsUnit.Timestamp);
+            command.Parameters.AddWithValue("@timestamp", gpsUnit.Timestamp);
 
             connection.Open();
             SqlDataReader reader = command.ExecuteReader();
